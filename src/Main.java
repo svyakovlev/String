2d3037f6adf4725a029ac53e54e52a6ce80876fd
Сергей <s.v.yakovlev@yandex.ru>
2022-06-23 21:49:37 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+
+//  Задача №1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+
+//  Задача №2
+        String fullNameUpperCase = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullNameUpperCase);
+
+//  Задача №3
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника - " + fullName.replace('ё', 'е'));
+    }
+}
